b40dfc967604a9b2d6f299eb52b3d5671da8abd8
channy929 <dbsys.kim@gmail.com>
2025-09-25 14:40:12 +0900

    Update ssh_example.py

--- ssh_example.py
+++ ssh_example.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     dag_id='ssh_example',
     start_date=datetime(2025, 1, 1),
-    schedule_interval=None,
+    schedule=None,
     catchup=False
 ) as dag:
     run_remote_command = SSHOperator(
